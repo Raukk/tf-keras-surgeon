556ead93b978bbbc08bb29dbf6bcde3221c8e5ac
Raukk <Raukk@users.noreply.github.com>
2019-06-22 15:08:10 -0500

    Completely Refactored the LeNet Example

--- src/tfkerassurgeon/examples/lenet_mnist.py
+++ src/tfkerassurgeon/examples/lenet_mnist.py
@@ -1,104 +1,184 @@
-from tensorflow.python.keras.layers import Dense, Conv2D, MaxPool2D, Flatten
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras import layers
-from tensorflow.python.keras import callbacks
-from tensorflow.examples.tutorials.mnist import input_data
-
-
 import sys
 sys.path.append("../../")
+
+import tensorflow as tf
+
+from tensorflow.python.keras import layers
+from tensorflow.python.keras import activations
+from tensorflow.python.keras import models
+from tensorflow.python.keras import backend
+from tensorflow.python.keras import callbacks
+from tensorflow.examples.tutorials import mnist
 
 import tfkerassurgeon
 from tfkerassurgeon import identify
 from tfkerassurgeon.operations import delete_channels
 
+print(tf.__version__)
 
-def main():
-    training_verbosity = 2
-    # Download data if needed and import.
-    mnist = input_data.read_data_sets('tempData', one_hot=True, reshape=False)
-    val_images = mnist.validation.images
-    val_labels = mnist.validation.labels
+
+# Set some static values that can be tweaked to experiment
+keras_verbosity = 2 # limits the printed output but still gets the Epoch stats
+epochs=200 # we'd never reach 200 because we have early stopping
+batch_size=128 # tweak this depending on your hardware and Model
+
+
+# Load dataset (it will automatically download it if needed)
+dataset = mnist.input_data.read_data_sets('tempData', one_hot=True, reshape=False)
+
+
+# Simple reusable shorthand to compile the model, so that we can be sure to use the same optomizer, loss, and metrics
+def compile_model(model):
+    
+    model.compile(optimizer='adam',
+                    loss='categorical_crossentropy',
+                    metrics=['accuracy'])
+
+
+
+# method that encapsulates the Models archeteture and construction
+def build_model():
 
     # Create LeNet model
-    model = Sequential()
-    model.add(Conv2D(20,
+    model = models.Sequential()
+    model.add(layers.Conv2D(20,
                      [3, 3],
                      input_shape=[28, 28, 1],
                      activation='relu',
                      name='conv_1'))
-    model.add(MaxPool2D())
-    model.add(Conv2D(50, [3, 3], activation='relu', name='conv_2'))
-    model.add(MaxPool2D())
+    model.add(layers.MaxPool2D())
+    model.add(layers.Conv2D(50, [3, 3], activation='relu', name='conv_2'))
+    model.add(layers.MaxPool2D())
     model.add(layers.Permute((2, 1, 3)))
-    model.add(Flatten())
-    model.add(Dense(500, activation='relu', name='dense_1'))
-    model.add(Dense(10, activation='softmax', name='dense_2'))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(500, activation='relu', name='dense_1'))
+    model.add(layers.Dense(10, activation='softmax', name='dense_2'))
 
-    model.compile(optimizer='adam',
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
+    compile_model(model)
 
-    early_stopping = callbacks.EarlyStopping(monitor='val_loss',
+    return model
+
+# a simple method that gets the callbacks for training
+def get_callbacks(use_early_stopping = True, use_reduce_lr = True):
+
+    callback_list = []
+
+    if(use_early_stopping):
+
+        callback_list.append(callbacks.EarlyStopping(monitor='val_loss',
                                              min_delta=0,
                                              patience=10,
-                                             verbose=training_verbosity,
-                                             mode='auto')
-    reduce_lr = callbacks.ReduceLROnPlateau(monitor='val_loss',
+                                             verbose=keras_verbosity,
+                                             mode='auto'))
+
+    if(use_reduce_lr):
+
+        callback_list.append(callbacks.ReduceLROnPlateau(monitor='val_loss',
                                             factor=0.1,
                                             patience=5,
-                                            verbose=training_verbosity,
+                                            verbose=keras_verbosity,
                                             mode='auto',
                                             epsilon=0.0001,
                                             cooldown=0,
-                                            min_lr=0)
+                                            min_lr=0))
 
-    # Train LeNet on MNIST
-    results = model.fit(mnist.train.images,
-                        mnist.train.labels,
-                        epochs=200,
-                        batch_size=128,
-                        verbose=2,
-                        validation_data=(val_images, val_labels),
-                        callbacks=[early_stopping, reduce_lr])
+    return callback_list
 
-    loss = model.evaluate(val_images, val_labels, batch_size=128, verbose=2)
+# and get the callbacks
+callback_list = get_callbacks()
+
+# Simple reusable shorthand for evaluating the model on the Validation set 
+def fit_model(model):
+    
+    return model.fit(
+                    dataset.train.images,
+                    dataset.train.labels,
+                    epochs=epochs,
+                    batch_size=batch_size,
+                    verbose=keras_verbosity,
+                    validation_data=(dataset.validation.images, dataset.validation.labels),
+                    callbacks=callback_list)
+
+# Simple reusable shorthand for evaluating the model on the Validation set 
+def eval_model(model):
+
+    return model.evaluate(
+                        dataset.validation.images, 
+                        dataset.validation.labels, 
+                        batch_size=batch_size, 
+                        verbose=keras_verbosity)
+
+
+# A helper that gets the layer by it's name 
+def prune_layer_by_name(model, layer_name):
+
+    # First we get the layer we are working on
+    layer = model.get_layer(name=layer_name)
+    # Then prune is and return the pruned model
+    return prune_layer(model, layer)
+
+
+
+
+# THIS IS WHERE THE MAGIC HAPPENS!
+# This method uses the Keras Surgeon to identify which parts od a layer can be pruned and then deletes them
+# Note: it returns the new, pruned model, that was recompiled
+def prune_layer(model, layer):
+    
+    # Get the APOZ (Average Percentage of Zeros) that should identify where we can prune
+    apoz = identify.get_apoz(model, layer, dataset.validation.images)
+
+    # Get the Channel Ids that have a high APOZ, which indicates they can be pruned
+    high_apoz_channels = identify.high_apoz(apoz)
+
+    # Run the pruning on the Model and get the Pruned (uncompiled) model as a result
+    model = delete_channels(model, layer, high_apoz_channels)
+
+    # Recompile the model
+    compile_model(model)
+
+    return model
+
+
+
+
+# the main function, that runs the training
+def main(): 
+
+    
+    # build the model
+    model = build_model()
+
+    # Initial Train on dataset
+    results = fit_model(model)
+
+    # eval and print the results of the training
+    loss = eval_model(model)
     print('original model loss:', loss, '\n')
-
-    layer_name = 'dense_1'
-
+    
+    # NOTE: This while true will continue until it ERRORs out because there is no escape condition.
     while True:
-        #duh this just keeps going until it crashes
-        layer = model.get_layer(name=layer_name)
-        apoz = identify.get_apoz(model, layer, val_images)
-        high_apoz_channels = identify.high_apoz(apoz)
-        model = delete_channels(model, layer, high_apoz_channels)
 
-        model.compile(optimizer='adam',
-                      loss='categorical_crossentropy',
-                      metrics=['accuracy'])
+        # only prune the Dense layer for this example
+        layer_name = 'dense_1'
+        # Run the Pruning on the layer
+        model = prune_layer_by_name(model, layer_name)
 
-        loss = model.evaluate(val_images,
-                              val_labels,
-                              batch_size=128,
-                              verbose=2)
+        # eval and print the results of the pruning
+        loss = eval_model(model)
         print('model loss after pruning: ', loss, '\n')
+        
+        # Retrain the model to accomodate for the changes
+        results = fit_model(model)
 
-        results = model.fit(mnist.train.images,
-                            mnist.train.labels,
-                            epochs=200,
-                            batch_size=128,
-                            verbose=training_verbosity,
-                            validation_data=(val_images, val_labels),
-                            callbacks=[early_stopping, reduce_lr])
-
-        loss = model.evaluate(val_images,
-                              val_labels,
-                              batch_size=128,
-                              verbose=2)
+        # eval and print the results of the retraining
+        loss = eval_model(model)
         print('model loss after retraining: ', loss, '\n')
 
+        # While TRUE will repeat until an ERROR occurs
 
+
+# Run the main Method
 if __name__ == '__main__':
     main()
 
